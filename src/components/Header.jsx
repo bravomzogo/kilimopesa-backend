d1620fdf0fd407105e28ae676995da1c5d6bdc44
bravolambo <bravomzogo@gmail.com>
2025-07-16 14:22:02 +0300

    d

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,11 +25,11 @@ const Header = () => {
 
   const navLinks = [
     { path: '/', name: 'Nyumbani' },
-    { path: 'api/products', name: 'Bidhaa' },
-    { path: '/api/land', name: 'Ardhi' },
-    { path: 'api/inputs', name: 'Pembejeo' },
-    { path: 'api/services', name: 'Huduma' },
-    { path: 'api/videos', name: 'Mafunzo' },
+    { path: '/products', name: 'Bidhaa' },
+    { path: '/land', name: 'Ardhi' },
+    { path: '/inputs', name: 'Pembejeo' },
+    { path: '/services', name: 'Huduma' },
+    { path: '/videos', name: 'Mafunzo' },
   ];
 
   const handleSearch = (e) => {
